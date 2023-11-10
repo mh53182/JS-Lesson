2621c042123b782d338089f8bd7f7adb7015ca85
mh53182 <marie.i.077145@gmail.com>
2023-11-10 01:55:40 +0000

    JSファイル整理

---
+++ Curriculum_8-10/script.js
@@ -0,0 +1,33 @@
+$(function() {
+  $('.menu-trigger').on('click', function(event) {
+    $(this).toggleClass('active');
+    $('#sp-menu').fadeToggle();
+    event.preventDefault();
+  });
+});
+
+
+// スムーススクロール
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
+
+
+
+// スワイパー
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
